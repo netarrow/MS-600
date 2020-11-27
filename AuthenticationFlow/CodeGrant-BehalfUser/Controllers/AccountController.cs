29fc95f21f2208b97c041b08f94f07595b2623a1
Matteo <>
2020-11-27 23:02:33 +0100

    fixed signout in be code grant. partially, in home user still seem authenticated

--- AuthenticationFlow/CodeGrant-BehalfUser/Controllers/AccountController.cs
+++ AuthenticationFlow/CodeGrant-BehalfUser/Controllers/AccountController.cs
@@ -1,8 +1,12 @@
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.Graph;
 using Microsoft.Identity.Web;
 
@@ -10,8 +14,13 @@ using Microsoft.Identity.Web;
 public class AccountController : Controller
 {
     [HttpGet]
+    public IActionResult SignOutSuccess() {
+        return View();
+    } 
+
+    [HttpGet]
     public IActionResult SignOut(string page)
     {
-        return new SignOutResult(new[] { "oidc", "Cookies" });          
+       return SignOut(new AuthenticationProperties() { RedirectUri = "/Account/SignOutSuccess" });
     }
 }
