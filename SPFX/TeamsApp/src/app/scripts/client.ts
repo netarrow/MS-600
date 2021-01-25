570c8cb61dbcd7037d597f100f7dd11d825493f6
Matteo <>
2021-01-25 19:45:57 +0100

    exploration of yo teams app creation

---
+++ SPFX/TeamsApp/src/app/scripts/client.ts
@@ -0,0 +1,10 @@
+// Default entry point for client scripts
+// Automatically generated
+// Please avoid from modifying to much...
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+export const render = (type: any, element: HTMLElement) => {
+    return ReactDOM.render(React.createElement(type, {}), element);
+};
+// Automatically added for the teamsAppTab tab
+export * from "./teamsAppTab/TeamsAppTab";
---
+++ SPFX/TeamsApp/src/app/teamsAppTab/TeamsAppTab.ts
@@ -0,0 +1,10 @@
+import { PreventIframe } from "express-msteams-host";
+
+/**
+ * Used as place holder for the decorators
+ */
+@PreventIframe("/teamsAppTab/index.html")
+
+
+export class TeamsAppTab {
+}
