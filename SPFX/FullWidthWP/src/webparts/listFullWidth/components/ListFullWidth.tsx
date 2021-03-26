994ca0bad3e1c48ce54f3a91020520e71381bd88
Matteo <>
2021-03-26 20:56:14 +0100

    setup a list in a fullwidth webpart

--- SPFX/FullWidthWP/src/webparts/listFullWidth/components/ListFullWidth.tsx
+++ SPFX/FullWidthWP/src/webparts/listFullWidth/components/ListFullWidth.tsx
@@ -2,20 +2,28 @@ import * as React from 'react';
 import styles from './ListFullWidth.module.scss';
 import { IListFullWidthProps } from './IListFullWidthProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { DetailsList, IColumn, buildColumns } from 'office-ui-fabric-react/lib/DetailsList';
 
 export default class ListFullWidth extends React.Component<IListFullWidthProps, {}> {
   public render(): React.ReactElement<IListFullWidthProps> {
+    const items = [{Id: 1, Nome: 'qq', Desc: 'asdf', Test: 'asdf',
+    Column: '', Another: 'asdfasdfasdf',
+    LongText: 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfsadfasfasdfsadfasdfasdfasdfasdfsadfsadfsadfasdf',
+    ColumnBu: 'qq'
+    }]
+    const columns = buildColumns(items);
     return (
       <div className={ styles.listFullWidth }>
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+            <DetailsList
+                className={ styles.list }
+                items={items}
+                setKey="set"
+                columns={columns}
+                ariaLabelForSelectionColumn="Toggle selection"
+                ariaLabelForSelectAllCheckbox="Toggle selection for all items" />
             </div>
           </div>
         </div>
