62ee754bc82ac225b52c581cc5e567fab77ee351
Matteo Tomasulo <matteo.tomasulo@teorema.net>
2021-04-27 18:05:58 +0200

    changed column s

--- SPFX/FullWidthWP/src/webparts/listFullWidth/components/ListFullWidth.tsx
+++ SPFX/FullWidthWP/src/webparts/listFullWidth/components/ListFullWidth.tsx
@@ -6,11 +6,15 @@ import { DetailsList, IColumn, buildColumns } from 'office-ui-fabric-react/lib/D
 
 export default class ListFullWidth extends React.Component<IListFullWidthProps, {}> {
   public render(): React.ReactElement<IListFullWidthProps> {
-    const items = [{Id: 1, Nome: 'qq',
-     Desc: 'asdf', Test: 'asdf',
-    Column: 'iuy', Another: 'asdfasdfasdf',
-    LongText: 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfsadfasdfsadfasfasdfsadfasdfasdfasdfasdfsadfsadfsadfasdf',
-    ColumnBu: 'qq'
+    const items = [{
+      Id: 1,
+      'Publication Number': 'qq',
+      'Applicant': 'asdf',
+      'Priority Number': 'asdf',
+      'Priority Date': 'iuy',
+      'Class': 'asdfasdfasdf',
+      'Title': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      'Comment': 'qq'
     }]
     let columns = buildColumns(items);
     columns.forEach(_ => _.isMultiline = true)
